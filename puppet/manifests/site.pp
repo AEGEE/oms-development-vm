b2acf1a36dadf8e4cae39d3708b35a9463d50416
grasshopper <bellicaf@tcd.ie>
2015-03-04 18:46:40 +0100

    gitignore ignores the creation of vagrant VM. manifest changed to reflect the LDAP base DN added stuff in ldap-files useful to have this running

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,17 +1,17 @@
 class { 'ldap::server::master':
-  suffix      => 'dc=foo,dc=bar',
+  suffix      => 'dc=aegee,dc=org',
   rootpw      => '{SSHA}o55pAdS3VSTvfd1RPSIEnHM2MvBlQdOt',
 }
 
 class { 'ldap::client':
   uri  => 'ldap://localhost',
-  base => 'dc=foo,dc=bar'
+  base => 'dc=aegee,dc=org'
 }
 
 class { 'phpldapadmin':
   ldap_host      => 'localhost',
-  ldap_suffix    => 'dc=foo,dc=bar',
-  ldap_bind_id   => 'cn=admin,dc=foo,dc=bar',
+  ldap_suffix    => 'dc=aegee,dc=org',
+  ldap_bind_id   => 'cn=admin,dc=aegee,dc=org',
   ldap_bind_pass => 'aegee', # TODO: not sure what this means
   require        => Class['ldap::server::master'],
 }
