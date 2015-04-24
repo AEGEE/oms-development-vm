a847edbec94eec6e9f1b53bfa1d5188426588c6b
grasshopper <bellicaf@tcd.ie>
2015-04-24 21:42:24 +0200

    closes #1

--- puppet/modules/aegee_db_files/files/import-log-indices.sh
+++ puppet/modules/aegee_db_files/files/import-log-indices.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-/usr/bin/ldapmodify -Q -c -Y EXTERNAL -H ldapi:/// -f /var/opt/aegee/1-log.ldif
-/usr/bin/ldapmodify -Q -c -Y EXTERNAL -H ldapi:/// -f /var/opt/aegee/2-indices.ldif
-
-# Nop to have clean exit code
-:
+/usr/bin/ldapmodify -Y EXTERNAL -H ldapi:/// -f /var/opt/aegee/1-log.ldif
+/usr/bin/ldapmodify -Y EXTERNAL -H ldapi:/// -f /var/opt/aegee/2-indices.ldif
--- puppet/modules/aegee_db_files/files/import-structure.sh
+++ puppet/modules/aegee_db_files/files/import-structure.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 #finally import the structure (people, antennae and so on):
-/usr/bin/ldapmodify -Q -c -Y EXTERNAL -H ldapi:/// -f /var/opt/aegee/10-structure.ldif
-/usr/bin/ldapmodify -Q -c -Y EXTERNAL -H ldapi:/// -f /var/opt/aegee/11-antennae.ldif
-/usr/bin/ldapmodify -Q -c -Y EXTERNAL -H ldapi:/// -f /var/opt/aegee/12-users.ldif
+/usr/bin/ldapadd -x -D "cn=admin,dc=aegee,dc=org" -w aegee -f /var/opt/aegee/10-structure.ldif
+/usr/bin/ldapadd -x -D "cn=admin,dc=aegee,dc=org" -w aegee -f /var/opt/aegee/11-antennae.ldif
+/usr/bin/ldapadd -x -D "cn=admin,dc=aegee,dc=org" -w aegee -f /var/opt/aegee/12-users.ldif
 
-# Nop to have clean exit code
-:
