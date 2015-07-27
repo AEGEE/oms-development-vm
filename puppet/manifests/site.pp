802369169a793abd52c6505a733d8afbd65d5f7d
Ingo Müller <ingo.mueller01@sap.com>
2015-07-27 10:15:30 +0200

    Add upstart service for OMS-core and manage it by puppet (issue #11).
    Also improve Node.js installation and improve doc of previous commit.

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -74,6 +74,8 @@ openldap::server::overlay { 'dynlist on o=aegee,c=eu':
   ensure => present,
 }
 
+# Install Node.js and npm
+# See https://github.com/puppet-community/puppet-nodejs
 class { 'nodejs':
   # TODO: The following setting installs nodejs from apt, which is not very
   #       recent. However, installing from NodeSource currently fails due to a
@@ -83,19 +85,23 @@ class { 'nodejs':
   manage_package_repo       => false,
   nodejs_dev_package_ensure => 'present',
   npm_package_ensure        => 'present',
+  legacy_debian_symlinks    => true,
 }
 
+# Clone OMS-core from git
 file { '/srv/oms-core':
   ensure => directory,
 }
 ->
 vcsrepo { '/srv/oms-core':
   ensure   => present,
-  revision => 'dev',
   provider => git,
   source   => 'https://bitbucket.org/aegeeitc/oms-core.git',
 }
 
+# Install OMS-core dependencies via npm
+# See http://howtonode.org/managing-module-dependencies and
+# https://docs.npmjs.com/cli/install
 # TODO: It would be great if there was a proper "puppet way" to do this.
 #       A feature request is tracked here:
 #       https://github.com/puppet-community/puppet-nodejs/issues/154
@@ -105,5 +111,26 @@ exec { 'install dependencies of oms-core':
   require => [ Class['nodejs'], Vcsrepo['/srv/oms-core'] ],
 }
 
-include phpldapadmin, aegee_db_files
-include othertools, git
+# Start OMS-core as an upstart service
+service { 'oms-core':
+  ensure   => running,
+  provider => 'upstart',
+  require  => [ Package['forever'],
+                File['/etc/init/oms-core.conf'],
+		Exec['install dependencies of oms-core'] ],
+}
+
+package { 'forever':
+  ensure   => 'present',
+  provider => 'npm',
+}
+
+# TODO: this should rather be provided by oms-core.git, so puppet would either
+#       create a link or copy the file to /etc/init/. At the very least, this
+#       file should not be with the other DB files.
+file { '/etc/init/oms-core.conf':
+  source => 'puppet:///modules/aegee_db_files/oms-core.conf',
+}
+
+include phpldapadmin, git
+include aegee_db_files, othertools
