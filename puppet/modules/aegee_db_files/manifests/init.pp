8a20224aa036f954e9660b97803ca86f036352af
Ingo Müller <ingo.mueller01@sap.com>
2015-10-11 15:32:27 +0200

    Remove dead code.
    Please don't check this in into master. Create private branches, keep
    it in your stash, whatever fits you best, but try to avoid noice for
    other people.

--- puppet/modules/aegee_db_files/manifests/init.pp
+++ puppet/modules/aegee_db_files/manifests/init.pp
@@ -1,25 +1,2 @@
 class aegee_db_files {
 }
-
-#class mongodb {
-#  class {'::mongodb::globals':
-#    manage_package_repo => true,
-#    bind_ip             => ["127.0.0.1"],
-#  }->
-#  class {'::mongodb::server':
-#    port    => 27017,
-#    verbose => true,
-#    ensure  => "present"
-#  }->
-#  class {'::mongodb::client': }
-#}
-#
-#class redis-cl {
-#  class { 'redis': }
-#}
-
-#include apt_update
-#include othertools
-#include nodejs
-#include mongodb
-#include redis-cl
