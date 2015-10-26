ad2e49de17e4b3384277466350e549490daf7fac
Ingo Müller <ingo.mueller@kit.edu>
2015-10-26 13:53:17 +0000

    Apply automatic fixes suggested by puppet-lint.

--- puppet/modules/aegee_ldap/manifests/init.pp
+++ puppet/modules/aegee_ldap/manifests/init.pp
@@ -7,26 +7,26 @@ class aegee_ldap (
 
   # Configure LDAP server
   class { 'openldap::server': }
-  openldap::server::database { "$dbname":
-    rootdn => "$rootdn",
-    rootpw => "$rootpw",
+  openldap::server::database { $dbname:
+    rootdn  => $rootdn,
+    rootpw  => $rootpw,
     backend => hdb,
-    ensure => present,
+    ensure  => present,
   }
 
   # Copy all DB files to node
-  file { "/var/opt/aegee":
-    ensure => "directory",
-    owner  => "root",
-    group  => "root",
-    mode   => 755,
+  file { '/var/opt/aegee':
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
     recurse => remote,
-    source  => "puppet:///modules/aegee_ldap/",
+    source  => 'puppet:///modules/aegee_ldap/',
   }
 
   # Import AEGEE schema and dependencies
-  $standardLdapSchemas = [ "core", "cosine", "dyngroup",
-                           "inetorgperson", "nis" ]
+  $standardLdapSchemas = [ 'core', 'cosine', 'dyngroup',
+                           'inetorgperson', 'nis' ]
   openldap::server::schema { $standardLdapSchemas:
     ensure  => present,
   }
@@ -35,36 +35,36 @@ class aegee_ldap (
     path    => '/var/opt/aegee/aegee.schema',
     require => [
                   File['/var/opt/aegee'],
-                  Openldap::Server::Database["$dbname"],
+                  Openldap::Server::Database[$dbname],
                   Openldap::Server::Schema[$standardLdapSchemas],
                ],
   }
 
   # Basic LDAP configuration
-  ldapdn { "enable logging":
-    dn                => "cn=config",
-    attributes        => ["olcLogLevel: $ldap_loglvel"],
-    unique_attributes => ["olcLogLevel"],
+  ldapdn { 'enable logging':
+    dn                => 'cn=config',
+    attributes        => ["olcLogLevel: ${ldap_loglvel}"],
+    unique_attributes => ['olcLogLevel'],
     ensure            => present,
   }
 
-  ldapdn { "enable index":
-    dn                => "olcDatabase={1}hdb,cn=config",
-    attributes        => ["olcDbIndex: uid eq,pres,sub"],
-    unique_attributes => ["olcDbIndex"],
+  ldapdn { 'enable index':
+    dn                => 'olcDatabase={1}hdb,cn=config',
+    attributes        => ['olcDbIndex: uid eq,pres,sub'],
+    unique_attributes => ['olcDbIndex'],
     ensure            => present,
-    require           => Openldap::Server::Database["$dbname"],
+    require           => Openldap::Server::Database[$dbname],
   }
 
   # Import test data (on first run)
   if $import_testdata {
-    exec { "import LDAP test data from LDIF files":
-      command => "/var/opt/aegee/testdata/import-data.sh $rootdn $rootpw",
+    exec { 'import LDAP test data from LDIF files':
+      command => "/var/opt/aegee/testdata/import-data.sh ${rootdn} ${rootpw}",
       creates => '/var/opt/aegee/testdata/.dataimported',
       unless  => '/usr/bin/test -f /var/opt/aegee/testdata/.dataimported',
       require =>  [
-                    Openldap::Server::Database["$dbname"],
-                    Openldap::Server::Schema["AEGEE"],
+                    Openldap::Server::Database[$dbname],
+                    Openldap::Server::Schema['AEGEE'],
                     File['/var/opt/aegee'],
                   ],
     }
@@ -74,10 +74,10 @@ class aegee_ldap (
   if $install_phpldapadmin {
     class { 'phpldapadmin':
       ldap_host      => 'localhost',
-      ldap_suffix    => "$dbname",
-      ldap_bind_id   => "$rootdn",
-      ldap_bind_pass => "$rootpw",
-      require        => Openldap::Server::Database["$dbname"],
+      ldap_suffix    => $dbname,
+      ldap_bind_id   => $rootdn,
+      ldap_bind_pass => $rootpw,
+      require        => Openldap::Server::Database[$dbname],
     }
   }
 
